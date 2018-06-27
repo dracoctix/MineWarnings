13a6fd012abcf73fb74d43d535d93e0224549b38
Yan BUATOIS <yanbuatois@outlook.fr>
2018-06-27 14:03:03 +0200

    Created warning base informations.

--- src/main/java/fr/dracoctix/dev/warnings/warnings/Warning.java
+++ src/main/java/fr/dracoctix/dev/warnings/warnings/Warning.java
@@ -1,4 +1,24 @@
 package fr.dracoctix.dev.warnings.warnings;
 
+import org.bukkit.entity.Player;
+
+import java.util.Date;
+
 public class Warning {
+    private Player joueur;
+    private Player moderator;
+    private Date start;
+    private int days;
+    private int points;
+    private String description;
+    private String justification;
+
+    public Warning(Player joueur, Player moderator, int days, int points, String description, String justification) {
+        this.joueur = joueur;
+        this.moderator = moderator;
+        this.days = days;
+        this.points = points;
+        this.description = description;
+        this.justification = justification;
+    }
 }
